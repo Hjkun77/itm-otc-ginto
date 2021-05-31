7e174d6778619df771cf355d6e54cd4fe3175422
Harvey Sison <harveyjaysison@gmail.com>
2021-06-01 02:08:53 +0800

    complete code

---
+++ app.py
@@ -0,0 +1,161 @@
+from flask import Flask,redirect
+from flask import render_template
+from flask import request
+from flask import session
+# from bson.json_util import loads, dumps
+from flask import make_response
+import database as db
+import authentication
+import ordermanagement as om
+import logging
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes.
+# Keep this really secret!
+app.secret_key = b's@g@d@c0ff33!'
+
+logging.basicConfig(level=logging.DEBUG)
+app.logger.setLevel(logging.INFO)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    is_successful, user = authentication.login(username, password)
+    app.logger.info('%s', is_successful)
+    if(is_successful):
+        session["user"] = user
+        return redirect('/products')
+    else:
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    session.pop("cart",None)
+    session.pop("order",None)
+    return redirect('/')
+
+@app.route('/')
+def index():
+    return render_template('index.html', page="Index")
+
+@app.route('/products')
+def products():
+    product_list = db.get_products()
+    return render_template('products.html', page="Products", product_list=product_list)
+
+@app.route('/productdetails')
+def productdetails():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+
+    return render_template('productdetails.html', code=code, product=product)
+
+@app.route('/branches')
+def branches():
+    branch_list = db.get_branches()
+    return render_template('branches.html', page="Branches", branch_list=branch_list)
+
+@app.route('/branchdetails')
+def branchdetails():
+    code = request.args.get('code', '')
+    branch = db.get_branch(int(code))
+
+    return render_template('branchdetails.html', page="Branches", branch=branch)
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html', page="About Us")
+
+@app.route('/addtocart')
+def addtocart():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+    item=dict()
+    # A click to add a product translates to a 
+    # quantity of 1 for now
+
+    item["qty"] =  1
+    item["name"] = product["name"]
+    item["code"] = product["code"]
+    item["subtotal"] = product["price"]*item["qty"]
+    item["image"] = product["image"]
+    item["price"] = product["price"]
+
+    if(session.get("cart") is None):
+        session["cart"]={}
+
+    cart = session["cart"]
+    cart[code]=item
+    session["cart"]=cart
+    return redirect('/cart')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+     return render_template('checkout.html')
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    order=dict()
+
+    firstName = request.form.get('firstName')
+    lastName = request.form.get('lastName')
+    address1 = request.form.get('address1')
+    address2 = request.form.get('address2')
+    city = request.form.get('city')
+    state = request.form.get('state')
+    zip = request.form.get('zip')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+    socmed = request.form.get('socmed')
+    notes = request.form.get('notes')
+
+    order["firstName"] =  firstName
+    order["lastName"] =  lastName
+    order["address1"] =  address1
+    order["address2"] =  address2
+    order["city"] =  city
+    order["state"] =  state
+    order["zip"] =  zip
+    order["phone"] =  phone
+    order["email"] =  email
+    order["socmed"] =  socmed
+    order["notes"] =  notes
+    
+    if(session.get("order") is None):
+        session["order"]={}
+
+    session["order"] = order
+    # clear cart in session memory upon checkout
+    om.create_order_from_cart()
+    # om.create_order()
+    session.pop("cart",None)
+    session.pop("order",None)
+    return redirect('/ordercomplete')
+
+@app.route('/ordercomplete')
+def ordercomplete():
+    return render_template('ordercomplete.html')
+
+@app.route('/api/products',methods=['GET'])
+def api_get_products():
+    resp = make_response( dumps(db.get_products()) )
+    resp.mimetype = 'application/json'
+    return resp
+    
+@app.route('/api/products/<int:code>',methods=['GET'])
+def api_get_product(code):
+    resp = make_response(dumps(db.get_product(code)))
+    resp.mimetype = 'application/json'
+    return resp
---
+++ database.py
@@ -0,0 +1,112 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+products_db = myclient["products"]
+order_management_db = myclient["order_management"]
+
+products = {
+    100: {"name":"Tea House","price":320,"image": "../static/images/tea-house.png", "description": "barley tea and malayco cake ice cream inspired by dim sum houses of Hong Kong, best for the adventurous"},
+    200: {"name":"Vietnamese Coffee","price":320, "image": "../static/images/vietnamese-coffee.png", "description": "boldly flavored ice cream crafted from Robusta beans and mixed with milky fudge chunks, ideal for coffee lovers"},
+    300: {"name":"White Rabbit","price":320, "image": "../static/images/white-rabbit.png", "description": "super creamy ice cream made from white rabbit candy, perfect for the nostalgic sweet tooth"},
+}
+
+branches = {
+    1: {"name":"Katipunan","phonenumber":"09179990000"},
+    2: {"name":"Tomas Morato","phonenumber":"09179990001"},
+    3: {"name":"Eastwood","phonenumber":"09179990002"},
+    4: {"name":"Tiendesitas","phonenumber":"09179990003"},
+    5: {"name":"Arcovia","phonenumber":"09179990004"},
+}
+
+users = {
+    "harveyjaysison@example.com":{"password":"12345678",
+                         "first_name":"Harvey",
+                         "last_name":"Sison"}
+}
+
+##  Products
+def get_product(code):
+    return products[code]
+
+def get_products():
+    product_list = []
+
+    for i,v in products.items():
+        product = v
+        product.setdefault("code",i)
+        product_list.append(product)
+
+    return product_list
+    
+# def get_products():
+#     product_list = []
+
+#     products_coll = products_db["products"]
+    
+#     for p in products_coll.find({}):
+#         product_list.append(p)
+
+#     print(product_list)
+#     return product_list
+
+# def get_product(code):
+#     products_coll = products_db["products"]
+
+#     product = products_coll.find_one({"code":code})
+
+#     return product
+
+## Production
+# def get_product(code):
+#     products_coll = products_db["products"]
+
+#     product = products_coll.find_one({"code":code},{"_id":0})
+
+#     return product
+
+# def get_products():
+#     product_list = []
+
+#     products_coll = products_db["products"]
+
+#     for p in products_coll.find({},{"_id":0}):
+#         product_list.append(p)
+
+#     return product_list
+
+
+## Branches
+def get_branch(code):
+    return branches[code]
+
+def get_branches():
+    branch_list = []
+
+    for i,v in branches.items():
+        branch = v
+        branch.setdefault("code",i)
+        branch_list.append(branch)
+
+    return branch_list
+
+## Users
+# def get_user(username):
+#     customers_coll = order_management_db['customers']
+#     user=customers_coll.find_one({"username":username})
+#     return user
+
+def get_user(username):
+    try:
+       return users[username]
+    except KeyError:
+       return None
+
+
+## Orders
+def create_order(order):
+    orders_coll = order_management_db['orders']
+    orders_coll.insert(order)
+
+
+
